55f0b5dc45560ce1f266674db3531efbb86c57c3
whwqkdakddl <112676975+whwqkdakddl@users.noreply.github.com>
2022-10-07 23:22:04 +0900

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,24 @@
+print("환영합니다.")
+환영합니다.
+print("파이썬의 세계에 오신 것을 환영합니다.")
+파이썬의 세계에 오신 것을 환영합니다.
+print("파이썬은 강력합니다.")
+파이썬은 강력합니다.
+
+반갑습니다. 파이썬!
+SyntaxError: invalid syntax
+0.6
+0.6
+Hello World!!!
+SyntaxError: invalid syntax
+print("한 주는",7*24","시간이다.")
+SyntaxError: unterminated string literal (detected at line 1)
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+
+t.fd(100);t.lt(90);t.fd(100);t.rt(90);t.fd(100);
+t.rt(90);t.fd(100);t.lt(90);t.fd(100)
+
+turtle.mainloop()
+turtle.bye()
